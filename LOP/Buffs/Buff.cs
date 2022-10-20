7cd25af282327828435f90f5596ea3cbbd864318
Lierick <3336970253@qq.com>
2022-10-20 21:43:16 +0800

    Lierick Test

---
+++ LOP/Buffs/Buff.cs
@@ -0,0 +1,37 @@
+﻿using LOP.Entities;
+using LOP.Skills;
+
+namespace LOP.Buffs
+{
+    public enum BuffType
+    {
+        Positive,
+        Negative,
+        Neutral,
+        Others
+    }
+    public class Buff
+    {
+        public Entity Entity { get; private set; }
+        public Entity OriginEntity { get; private set; }
+        public Skill OriginSkill { get; private set; }
+        public String Name { get; protected set; }
+        public BuffType Type { get; protected set; }
+        public Buff(Entity entity,Entity originEntity, Skill originSkill)
+        {
+            Name = "Default Buff";
+            Type = BuffType.Others;
+            Entity = entity;
+            OriginEntity = originEntity;
+            OriginSkill = originSkill;
+        }
+        public virtual void OnCalcDamageAsSource() { }
+        public virtual void OnCalcDamageAsTarget() { }
+        public virtual void OnObtain() { }
+        public virtual void Update() { }
+        public virtual void OnLose() { }
+        public virtual void OnUseSkill() { }
+        public virtual void OnNormalAttack() { }
+        public virtual void OnGetBuff(Buff buff) { }
+    }
+}
--- LOP/Skills/Skills.cs
+++ LOP/Skills/Skills.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LOP.Core;
+using LOP.Entities;
 
 namespace LOP.Skills
 {
-    public class Skill
+    public class Skill : IUpdatable
     {
+        public Entity Entity { get; private set; }
+        public float CoolDown;
+        public Skill(Entity entity)
+        {
+            Entity = entity;
+        }
+        public virtual void Update()
+        {
+            CoolDown -= Time.DeltaTime;
+            if (CoolDown < 0)
+            {
+                CoolDown = 0;
+            }
+        }
+        public virtual void OnEquipped() {}
+        public virtual void OnOccur() {}
     }
 }
