d960920c59787cffe6aeb4df3cbda50fdb13fb84
Lierick <3336970253@qq.com>
2022-10-20 15:22:19 +0800

    Lierick Test

---
+++ LOP/MainUpdater.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LOP
+{
+    public class MainUpdater
+    {
+        private static MainUpdater instance;
+        public static MainUpdater Instance
+        {
+            get { return instance ??= new MainUpdater(); }
+        }
+        
+    }
+}
