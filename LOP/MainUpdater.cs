262c30677c0e5c49da6734211e6285a8485d5cfa
Lierick <3336970253@qq.com>
2022-10-20 16:36:38 +0800

    Lierick Test

--- LOP/MainUpdater.cs
+++ LOP/MainUpdater.cs
@@ -1,15 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace LOP
 {
-    public class MainUpdater
+    public class MainUpdater : IUpdatable
     {
         private static MainUpdater instance;
         public static MainUpdater Instance
         {
             get { return instance ??= new MainUpdater(); }
         }
-        
+        private List<Script> scripts = new List<Script>();
+        public static void AddScript(Script script)
+        {
+            Instance.scripts.Add(script);
+        }
+        public static void RemoveScript(Script script)
+        {
+            if (Instance.scripts.Contains(script)) {
+                script.OnDestroy();
+                Instance.scripts.Remove(script);
+            }
+        }
+        private MainUpdater()
+        {
+            scripts = new List<Script>();
+        }
+        public void Init()
+        {
+            Type scriptType = typeof(Script);
+            var types = Assembly.GetCallingAssembly().GetTypes();
+            foreach (var type in types)
+            {
+                var parent = type.BaseType;
+                Console.WriteLine("Init: " + type.FullName);
+                if (parent != null && parent.FullName == scriptType.FullName)
+                {
+                    Script script = Activator.CreateInstance(type) as Script;
+                    if (script != null) AddScript(script);
+                }
+            }
+        }
+        public void Start()
+        {
+            foreach (var script in scripts)
+            {
+                script.Start();
+            }
+        }
+        public void Update()
+        {
+            foreach (var script in scripts)
+            {
+                script.Update();
+            }
+        }
+    }
+    public abstract class Script : IUpdatable
+    {
+        public virtual void Start() { }
+        public virtual void Update() { }
+        public virtual void OnDestroy() { }
+        public virtual void Destroy()
+        {
+            MainUpdater.RemoveScript(this);
+        }
+        public static void Destroy(Script script)
+        {
+            script.Destroy();
+        }
+    }
+    public class TestScript : Script
+    {
+        public override void Update()
+        {
+            Console.WriteLine("TestPass!");
+        }
     }
 }
--- LOP/Program.cs
+++ LOP/Program.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace LOP
 {
     internal static class Program
@@ -5,13 +7,32 @@ namespace LOP
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
+        [DllImport("kernel32.dll")]
+        static extern bool FreeConsole();
+        [DllImport("kernel32.dll")]
+        public static extern bool AllocConsole();
         [STAThread]
         static void Main()
         {
+            AllocConsole();
+            MainUpdater.Instance.Init();
+            MainUpdater.Instance.Start();
+            Thread updateThread = new Thread(new ThreadStart(update));
+            updateThread.Start();
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+            FreeConsole();
+            Environment.Exit(0);
+        }
+        private static void update()
+        {
+            while(true)
+            {
+                MainUpdater.Instance.Update();
+                Thread.CurrentThread.Join(100);
+            }
         }
     }
 }
