d16de7fa9523c8113b9c994a76f73ec2bbbc4f28
Amptyz <a542674348@gmail.com>
2022-10-20 14:29:09 +0800

    Lierick Test

---
+++ LOP/Entity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOP
+{
+    public abstract class Entity : IUpdatable
+    {
+        public abstract void Update();
+    }
+
+    public class EntityData
+    {
+        public int MaxHealth;
+        public int Health;
+        public Entity Entity { get; private set; }
+        public EntityData(Entity entity)
+        {
+            Entity = entity;
+        }
+        public virtual EntityData Copy()
+        {
+            return new EntityData(Entity);
+        }
+    }
+
+}
