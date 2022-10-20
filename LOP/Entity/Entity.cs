d4459c603ceaa83f80aa7acea7c7fba3b049f450
Amptyz <a542674348@gmail.com>
2022-10-20 15:21:15 +0800

    Lierick Test

--- LOP/Entity/Entity.cs
+++ LOP/Entity/Entity.cs
@@ -4,17 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LOP
+namespace LOP.Entity
 {
     public abstract class Entity : IUpdatable
     {
+        public EntityData EntityData { get; private set; }
+        public Entity()
+        {
+            EntityData = new EntityData(this); 
+        }
         public abstract void Update();
     }
-
+    public enum Nature { Fire, Water, Grass, Normal };
     public class EntityData
     {
+       
         public int MaxHealth;
         public int Health;
+        public int Swiftness;
+        public Nature nature;
+
         public Entity Entity { get; private set; }
         public EntityData(Entity entity)
         {
