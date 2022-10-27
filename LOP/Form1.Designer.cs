d00cc74df1f3f97242f30f745c49d058de0b6164
Lierick <3336970253@qq.com>
2022-10-27 23:09:36 +0800

    Lierick Test

--- LOP/Form1.Designer.cs
+++ LOP/Form1.Designer.cs
@@ -32,9 +32,10 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(878, 621);
+            this.ClientSize = new System.Drawing.Size(718, 518);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
